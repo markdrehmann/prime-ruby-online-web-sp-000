eeb067021bba2175b6f3e99d41c9ae1c60be7924
Mark Drehmann <markdrehmann@gmail.com>
2020-01-30 00:40:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(n)
+  numbers = (2..n).to_a
+    if n < 2
+      return false
+    end
+end
+
+
+# if i <= 1, false
+# check if any numbers are factors up to square root of i
