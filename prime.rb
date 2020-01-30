e56cb1dd1ca19634f6c1db03baca258919aa565d
Mark Drehmann <markdrehmann@gmail.com>
2020-01-30 01:40:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+  if n > 1
+  numbers = (2..n/2).to_a
+  numbers.none? do |num|
+    n % num == 0
+    end
+  else
+    false
+  end
+end
+
+
+# if n <= 1, false
+# check if any numbers are factors up to square root of i, or n/2...
